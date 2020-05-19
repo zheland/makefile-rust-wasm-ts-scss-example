3c57d9a6c26a8a1782cb22b4f8de0044f40d4e03
Andrey Zheleznov <zheland.net@gmail.com>
2020-05-19 18:38:30 +0300

    Use seperate rust/wasm build profiles for dev and release server builds

--- www/scripts/build-wasm.sh
+++ www/scripts/build-wasm.sh
@@ -4,12 +4,14 @@ set -e
 
 module="$1"
 target="$2"
+profile="$3"
+output="$4"
 (
-    cd ../wasm-"$module";
-    cargo -q build --release --target wasm32-unknown-unknown
+    cd "../wasm-$module"
+    cargo -q build $profile --target wasm32-unknown-unknown
 )
 
-source="../target/wasm32-unknown-unknown/release/wasm_$module.wasm"
+source="../target/wasm32-unknown-unknown/$output/wasm_$module.wasm"
 if [[ "$source" -nt "$target" ]]; then
     cp "$source" "$target"
     exit 0
