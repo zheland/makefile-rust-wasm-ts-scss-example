f6db978652c9d52ad5e8573d251e1def9d5bfc61
Andrey Zheleznov <zheland.net@gmail.com>
2020-06-06 13:37:14 +0300

    Fix excess output in setup script

--- setup.sh
+++ setup.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-if ! type "rustup" > /dev/null; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-fi
-if type "apt" > /dev/null; then
-    if ! type "npm" > /dev/null; then
-        curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-        sudo apt install -y nodejs
-    fi
-    if ! type "make" > /dev/null; then
-        sudo apt install -y build-essential
-    fi
-fi
+function has() {
+    type $1 > /dev/null 2>&1
+}
 
-rustup self update
-rustup update
-rustup target add wasm32-unknown-unknown
-cargo install wasm-pack
-cargo install wasm-bindgen-cli
+function run() {
+    echo "+ $*"
+    "$@"
+}
 
-if type "sudo" > /dev/null; then
-    npm_local_version=$(npm list -g npm --depth=0 | grep -oP "(?<=@)[\w-\.]*")
-    npm_last_version=$(npm show npm version)
-    if [ "$npm_local_version" != "$npm_last_version" ]; then
-        sudo npm install -g npm@latest
-    fi
-fi
+! has "rustup" && {
+    run curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
+has "sudo" && has "apt" && ! has "npm" && {
+    run curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+    run sudo apt install -y nodejs
+}
+has "sudo" && has "apt" && ! has "make" && {
+    run sudo apt install -y build-essential
+}
+
+run rustup self update
+run rustup update
+run rustup target add wasm32-unknown-unknown
+run cargo install wasm-pack
+run cargo install wasm-bindgen-cli
+
+npm_local_version=$(npm list -g npm --depth=0 | grep -oP "(?<=@)[\w-\.]*")
+npm_last_version=$(npm show npm version)
+has "sudo" && [ "$npm_local_version" != "$npm_last_version" ] && {
+    run sudo npm install -g npm@latest
+}
 
 ( cd www && npm install )
